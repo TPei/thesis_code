b2910f22da4a9cdda3a4103830e333b11a5c26e1
TPei <t.s.peikert@gmail.com>
2019-02-09 18:31:41 +0100

    add function for handling incoming cloudant queries

---
+++ functions/apis/handle_cloudant_query/main.js
@@ -0,0 +1,14 @@
+// endpoint called when querying the service
+// receives a cloudant query and some additional data
+function main (params) {
+  // TODO: handle cloudant query
+  const query = params.data.query
+
+  return {
+    data: {
+      query: query,
+      access_purpose: params.data.access_purpose,
+      access_utilizer: params.data.access_utilizer
+    }
+  }
+}
