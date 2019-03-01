a34179c2362f253489c07546e37a0442586798a3
TPei <t.s.peikert@gmail.com>
2019-03-01 16:31:32 +0100

    "streamline" fetch_user_data to do only one query and then consolidate data requirements (slightly faster)

--- functions/cloudant/fetch_user_data/main.js
+++ functions/cloudant/fetch_user_data/main.js
@@ -1,31 +1,45 @@
 const openwhisk = require('openwhisk')
+const _ = require('lodash');
 
 function main(params) {
   const ow = openwhisk()
 
-  let results = params.data.map(function(params) {
-    let fields = params.data.fields
+  // not faster than nested loop, but nicer to read...
+  let allRequiredFields = params.data.map(ele => ele.data.fields)
+  allRequiredFields = _.flattenDeep(allRequiredFields)
+  allRequiredFields = _.uniq(allRequiredFields)
 
-    return ow.actions.invoke({
-      "name": "applicationdb/exec-query-find",
-      "blocking": true,
-      "result": true,
-      "params": {
-        "dbname": "user",
-        "query": {
-          "selector": {
-            "user_id": params.data.id.toString()
-          },
-          "fields": fields
-        }
+  return ow.actions.invoke({
+    "name": "applicationdb/exec-query-find",
+    "blocking": true,
+    "result": true,
+    "params": {
+      "dbname": "user",
+      "query": {
+        "selector": {
+          "user_id": { "$in" : params.data.map(ele => ele.data.id) }
+        },
+        "fields": allRequiredFields.concat("user_id") // for mapping
       }
-    }).then(function(res) {
-      return { data: res.docs[0], passthrough: params.passthrough }
-    })
-  })
+    }
+  }).then(function(res) {
+    return {
+      data: res.docs.map(document => {
+        // find passthrough data for document
+        let user_id = document.user_id
+        let data = params.data.find(ele => { return ele.data.id === user_id })
 
-  return Promise.all(results).then(results => { return { data: results } })
+        // only allow data for object that was allowed before
+        let documentData = {}
+        for (var key in document) {
+          if (document.hasOwnProperty(key) && data.data.fields.includes(key)) {
+            documentData[key] = document[key];
+          }
+        }
+        return { data: documentData, passthrough: data.passthrough }
+      })
+    }
+  })
 }
 
 exports.handler = main;
-
