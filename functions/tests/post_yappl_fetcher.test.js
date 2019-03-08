09c34a12b8ef18dbca724db38afa1080a826fe28
TPei <t.s.peikert@gmail.com>
2019-03-08 08:37:03 +0100

    clean up yappl fetch pre/post

--- functions/tests/post_yappl_fetcher.test.js
+++ functions/tests/post_yappl_fetcher.test.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const transform = require('../cloudant/post_yappl_fetcher/main.js').handler
+const transform = require('../cloudant/fetch_yappl_for_user/post.js').handler
 
 // test that all request.json / output.json files match
 test('handle cloudant query post does its job', () => {
-  let input = '../cloudant/post_yappl_fetcher/request.json'
-  let output = '../cloudant/post_yappl_fetcher/output.json'
+  let input = '../cloudant/fetch_yappl_for_user/post_request.json'
+  let output = '../cloudant/fetch_yappl_for_user/post_output.json'
 
   output = JSON.parse(fs.readFileSync(output, 'utf8'));
   input = JSON.parse(fs.readFileSync(input, 'utf8'));
--- functions/tests/pre_yappl_fetcher.test.js
+++ functions/tests/pre_yappl_fetcher.test.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const transform = require('../cloudant/fetch_yappl_for_user/main.js').handler
+const transform = require('../cloudant/fetch_yappl_for_user/pre.js').handler
 
 // test that all request.json / output.json files match
 test('handle cloudant query post does its job', () => {
-  let input = '../cloudant/fetch_yappl_for_user/request.json'
-  let output = '../cloudant/fetch_yappl_for_user/output.json'
+  let input = '../cloudant/fetch_yappl_for_user/pre_request.json'
+  let output = '../cloudant/fetch_yappl_for_user/pre_output.json'
 
   output = JSON.parse(fs.readFileSync(output, 'utf8'));
   input = JSON.parse(fs.readFileSync(input, 'utf8'));
