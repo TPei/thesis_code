2d34ce44b9deda13ec47a7f616f3ed97e2b09b8e
TPei <t.s.peikert@gmail.com>
2019-03-11 11:59:27 +0100

    clean up packages, add postgres query function

---
+++ functions/postgres_query/main.js
@@ -0,0 +1,25 @@
+const { Client } = require('pg')
+
+function main(params) {
+
+  var client = new Client({
+    user: params.user,
+    password: params.password,
+    database: params.dbname,
+    port: params.port,
+    host: params.host,
+    ssl: true
+  });
+  client.connect()
+
+  return client.query(params.query).then(res => {
+    client.end()
+    return { response: res.rows }
+  }).catch(err => {
+    client.end()
+    return { error: err }
+  })
+}
+
+exports.main = main;
+
