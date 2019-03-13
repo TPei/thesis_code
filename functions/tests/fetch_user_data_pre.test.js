44b52ec2c0c6d8d7ae72fa8c6700cf392da0dffc
TPei <t.s.peikert@gmail.com>
2019-03-13 09:13:28 +0100

    fix lodash dependency in test

--- functions/tests/fetch_user_data_pre.test.js
+++ functions/tests/fetch_user_data_pre.test.js
@@ -1,17 +1,12 @@
-// DOESN'T WORK BECAUSE OF LODASH INSTALLATION ISSUES /shrug
+const fs = require('fs');
+const transform = require('../cloudant/fetch_user_data/pre.js').handler
 
-//const fs = require('fs');
-//const transform = require('../cloudant/fetch_user_data/pre.js').handler
+// test that all request.json / output.json files match
+test('user_fetch_preparer does its job', () => {
+  let input = '../cloudant/fetch_user_data/pre_request.json'
+  let output = '../cloudant/fetch_user_data/pre_output.json'
 
-//// test that all request.json / output.json files match
-//test('user_fetch_preparer does its job', () => {
-  //let input = '../cloudant/fetch_user_data/pre_request.json'
-  //let output = '../cloudant/fetch_user_data/pre_output.json'
-
-  //output = JSON.parse(fs.readFileSync(output, 'utf8'));
-  //input = JSON.parse(fs.readFileSync(input, 'utf8'));
-  //expect(transform(input)).toEqual(output)
-//});
-
-test('nothing', () => {
+  output = JSON.parse(fs.readFileSync(output, 'utf8'));
+  input = JSON.parse(fs.readFileSync(input, 'utf8'));
+  expect(transform(input)).toEqual(output)
 });
