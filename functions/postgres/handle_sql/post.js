e17b8f8707b84d451e452ecede7a7007a8c3a70d
TPei <t.s.peikert@gmail.com>
2019-03-14 07:40:02 +0100

    cast ids to string when returning from postgres

--- functions/postgres/handle_sql/post.js
+++ functions/postgres/handle_sql/post.js
@@ -1,7 +1,7 @@
 function main(params) {
   return {
     data: {
-      ids: params.docs.map(user => user.user_id),
+      ids: params.docs.map(user => user.user_id.toString()),
       query: params.passthrough.query,
       access_purpose: params.passthrough.access_purpose,
       access_utilizer: params.passthrough.access_utilizer
