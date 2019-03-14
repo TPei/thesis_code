df914836107dfd00e7328a7b9bbfa8675d50fcd4
TPei <t.s.peikert@gmail.com>
2019-03-14 08:48:08 +0100

    add spec

--- functions/tests/user_fetch_preparer.test.js
+++ functions/tests/user_fetch_preparer.test.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const transform = require('../user_fetch_preparer/main.js').handler
+const transform = require('../cloudant/user_fetch_preparer/main.js').handler
 
 // test that all request.json / output.json files match
 test('user_fetch_preparer does its job', () => {
-  let input = '../user_fetch_preparer/request.json'
-  let output = '../user_fetch_preparer/output.json'
+  let input = '../cloudant/user_fetch_preparer/request.json'
+  let output = '../cloudant/user_fetch_preparer/output.json'
 
   output = JSON.parse(fs.readFileSync(output, 'utf8'));
   input = JSON.parse(fs.readFileSync(input, 'utf8'));
